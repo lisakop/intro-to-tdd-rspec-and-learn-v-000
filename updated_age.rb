d196b7cb9b6e0e5493f92365fe0b2888b4ee3813
Lisa Koperski <lisa_koperski_junk@yahoo.com>
2017-10-06 22:55:20 +0000

    automatically backed up by learn

--- updated_age.rb
+++ updated_age.rb
@@ -1,6 +1,6 @@
 require_relative '../current_age_for_birth_year_spec.rb'
 
-describe "current_age_for_birth_year method" do
+describe "current_age_for_birth_year_spec method" do
    it "returns the age of a person based on the year of birth" do
       current_year = Time.now.year
       birth_year = 1984
@@ -9,7 +9,7 @@ describe "current_age_for_birth_year method" do
       def age_of_person
       end
 
-      age_of_person = current_age_for_birth_year(birth_year)
+      age_of_person = current_age_for_birth_year_spec(birth_year)
       expect(age_of_person).to eq(answer)
    end
 end
