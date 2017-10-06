6c6b1a5cfccbbbf9448e8c3098d6952d59c3a813
Lisa Koperski <lisa_koperski_junk@yahoo.com>
2017-10-06 22:53:34 +0000

    automatically backed up by learn

--- updated_age.rb
+++ updated_age.rb
@@ -6,7 +6,8 @@ describe "current_age_for_birth_year method" do
       birth_year = 1984
       answer = current_year - birth_year
 
-      def current_age_for_birth_year(current_year - birth_year)
+      def age_of_person
+      end
 
       age_of_person = current_age_for_birth_year(birth_year)
       expect(age_of_person).to eq(answer)
